68ec44a85eaaf9d6a379dd6ec7f98dfeb344d0e4
bm7at <bmayer710@gmail.com>
2021-05-17 00:42:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ function saveLead() {
     console.log("Button clicked!")
 }
 */
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 
-let inputBtn = document.getElementById("input-btn")
+//let inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function(){
-    console.log("Button clicked from addEventl")
+    //myLeads.push("www.awesomelead.com")
+
+    myLeads.push(inputEl.value)
+
+    console.log(myLeads)
 })
