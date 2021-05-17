4892aaecd5c523719f840bee1d9866e31b846a0f
bm7at <bmayer710@gmail.com>
2021-05-17 22:14:26 +0200

    updated index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function saveLead() {
     console.log("Button clicked!")
 }
 */
+/*
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -16,3 +17,33 @@ inputBtn.addEventListener("click", function(){
 
     console.log(myLeads)
 })
+*/
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+
+    renderLeads()
+})
+
+// Let's try a different method!
+function renderLeads() {
+let listItems = ""
+for (let i = 0; i < myLeads.length; i++) {
+
+    listItems += "<li>" + myLeads[i] + "</li>"
+    /*
+    const li = document.createElement("li")
+    li.textContent = myLeads[i]
+    ulEl.append(li)
+    */
+   console.log(listItems)
+}
+
+ulEl.innerHTML = listItems
+}
