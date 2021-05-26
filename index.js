d48ea1fa7a1c98b2cd7f28a21554c56970e909ec
bm7at <bmayer710@gmail.com>
2021-05-26 23:10:19 +0200

    added localstorage

--- index.js
+++ index.js
@@ -20,15 +20,29 @@ inputBtn.addEventListener("click", function(){
 */
 
 let myLeads = []
+
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+//localStorage.clear()
+let leads = JSON.parse(localStorage.getItem("myLeads"));
+
+
+
+
+console.log(leads)
+
 inputBtn.addEventListener("click", function() {
+
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
 
     renderLeads()
+
+    console.log(localStorage.getItem("myLeads"))
 })
 
 // Let's try a different method!
